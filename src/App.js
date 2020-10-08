c8f30dc8fad011df5dee4244a49f4ad75f4ee665
dev-wina <debugingsy@gmail.com>
2020-10-08 12:08:57 +0900

    deploy test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          변동 사항 적용 완료
         </a>
       </header>
     </div>
